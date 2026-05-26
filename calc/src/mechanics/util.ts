5c289709ddbcb587834f39eda788187b5c4ce0c4
Ryan T. <radicalryan75@gmail.com>
2026-05-26 00:11:17 -0400

    this should be the last fix

--- calc/src/mechanics/util.ts
+++ calc/src/mechanics/util.ts
@@ -723,10 +723,10 @@ export function addSpacedStr(a: string | undefined, b: string | undefined, c: Ra
   console.log(c);
   if (b) {
     if (d === 'a') {
-      if (c.attackerAbilityList) { c.attackerAbilityList.push(b); }
+      if (c.attackerAbilityList && !c.attackerAbilityList.includes(b)) { c.attackerAbilityList.push(b); }
       else { c.attackerAbilityList = [b]; }
     } else {
-      if (c.defenderAbilityList) { c.defenderAbilityList.push(b); }
+      if (c.defenderAbilityList && !c.defenderAbilityList.includes(b)) { c.defenderAbilityList.push(b); }
       else { c.defenderAbilityList = [b]; }
     }
     return a ? a + ' ' + b : b;
