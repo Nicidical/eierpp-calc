6eeda904af0a5c05bee8dead3b38f2e75b68654d
Ryan T. <radicalryan75@gmail.com>
2026-05-25 23:44:46 -0400

    Update util.ts

--- calc/src/mechanics/util.ts
+++ calc/src/mechanics/util.ts
@@ -722,6 +722,7 @@ export function OF32(n: number) {
 export function addSpacedStr(a: string | undefined, b: string | undefined, c: string[] | undefined) {
   if (b && ((c && !c.includes(b)) || !c)) {
     if (c) { console.log(c); c.push(b); }
+    else { c = [b]; }
     return a ? a + ' ' + b : b;
   }
   return '';
